a7044c7441f37d676db1e43c280fd419999a3588
w4ffl35 (Joe Curlee) <25737761+w4ffl35@users.noreply.github.com>
2022-12-19 13:02:09 -0700

    Allow max value on line edit

--- krita_stable_diffusion/interface/widgets/line_edit.py
+++ krita_stable_diffusion/interface/widgets/line_edit.py
@@ -9,7 +9,15 @@ class LineEdit(Widget):
     :param placeholder: The placeholder text of the line edit
     :param config_name: The name of the config to be set when the line edit is changed
     """
-    config_name = None
+    def __init__(self, *args, **kwargs):
+        self.max = kwargs.pop("max", None)
+        super().__init__(*args, **kwargs)
+
+    def update_value(self, config_name, value):
+        if self.max and value and value != "" and int(value) > self.max:
+            value = self.max
+            self.widget.setText(str(value))
+        super().update_value(config_name, value)
 
     def create_widget(self):
         element = QLineEdit()
@@ -18,5 +26,5 @@ class LineEdit(Widget):
             element.textChanged.connect(
                 lambda: self.update_value(self.config_name, element.text())
             )
-            element.setText(self.config.value(self.config_name))
+            element.setText(str(self.config.value(self.config_name)))
         self.widget = self.initialize_widget(element)
