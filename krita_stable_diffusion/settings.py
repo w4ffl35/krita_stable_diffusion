b09573e81604d33aef6b3dd822ee1d71375d1bac
w4ffl35 (Joe Curlee) <25737761+w4ffl35@users.noreply.github.com>
2022-12-19 13:03:41 -0700

    Adds available scheduler to settings

--- krita_stable_diffusion/settings.py
+++ krita_stable_diffusion/settings.py
@@ -1,5 +1,13 @@
 APPLICATION_ID = "krita_stable_diffusion"
-SAMPLERS = ["DDIM", "PLMS",]# 'k_dpm_2_a', 'k_dpm_2', 'k_euler_a', 'k_euler', 'k_heun', 'k_lms']
+SCHEDULERS = [
+    "euler_a",
+    "euler",
+    "ddpm",
+    "ddim",
+    "plms",
+    "lms",
+    "dpm"
+]
 UPSCALERS = ["None", "Lanczos"]
 MODELS = [
     "stable-diffusion-2-1-base",
