ab5c61ac3bcdbdb6098e36c87380f847f080199c
w4ffl35 (Joe Curlee) <25737761+w4ffl35@users.noreply.github.com>
2023-01-04 02:57:58 -0700

    Adds model base path for custom model handling

--- krita_stable_diffusion/settings.py
+++ krita_stable_diffusion/settings.py
@@ -15,17 +15,17 @@ MODEL_VERSIONS = [
 ]
 MODELS = {
     "v1": [
-        "runwayml/stable-diffusion-v1-5",
-        "runwayml/stable-diffusion-inpainting",
+        # "stable-diffusion-v1-5",
+        # "stable-diffusion-inpainting",
     ],
     "v2": [
-        "stabilityai/stable-diffusion-2-1-base",
-        "stabilityai/stable-diffusion-x4-upscaler",
-        "stabilityai/stable-diffusion-2-inpainting",
-        "stabilityai/stable-diffusion-2-depth",
+        # "stable-diffusion-2-1-base",
+        # "stable-diffusion-x4-upscaler",
+        # "stable-diffusion-2-inpainting",
+        # "stable-diffusion-2-depth",
     ]
 }
-DEFAULT_MODEL = MODELS["v2"][0]
+DEFAULT_MODEL = MODELS["v2"][0] if len(MODELS["v2"]) > 0 else None
 DEFAULT_SCHEDULER = SCHEDULERS[0]
 MIN_SEED = 4294967295#0
 MAX_SEED = 4294967295
